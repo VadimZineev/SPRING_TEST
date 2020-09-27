5aad1e71c22568b5734b248da583429d0a1b1407
Vadim <vadimz026@gmail.com>
2020-09-27 16:02:30 +0300

    let's start

---
+++ SpringTestProject270920/src/main/java/com/example/SpringTestProject270920/SpringTestProject270920Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringTestProject270920;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestProject270920Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestProject270920Application.class, args);
+	}
+
+}
---
+++ SpringTestProject270920/src/test/java/com/example/SpringTestProject270920/SpringTestProject270920ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringTestProject270920;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestProject270920ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
